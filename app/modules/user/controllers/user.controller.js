5e12049daa3960347c3285ba6bf4b961a6c3c32f
imasr <ashishs723@gmail.com>
2018-10-23 11:12:04 +0530

    fix

--- app/modules/user/controllers/user.controller.js
+++ app/modules/user/controllers/user.controller.js
@@ -2,23 +2,21 @@ const user = require("../business/user.business");
 const { errorHandler } = require("../../../helpers/errorHandling.helper");
 
 // register a new user
-exports.user_register = async (req, res) => {
-    try {
-        let register = await user.registration(req.body)
-        if (register)
-            res.status(200).send(register);
-    } catch (e) {
-        res.status(400).send(errorHandler(e));
-    }
+exports.user_register = (req, res) => {
+    return user.registration(req.body)
+        .then(result => {
+            res.status(200).send(result);
+        }).catch(e => {
+            res.status(400).send(errorHandler(e));
+        });;
 };
 
 // login an existing user and generate refresh jwt token
-exports.user_login = async (req, res) => {
-    try {
-        let login = await user.login(req.body)
-        if (login)
-            res.status(200).send(login);
-    } catch (e) {
-        res.status(400).send(errorHandler(e));
-    }
+exports.user_login = (req, res) => {
+    return user.login(req.body)
+        .then(result => {
+            res.status(200).send(result);
+        }).catch(e => {
+            res.status(400).send(errorHandler(e));
+        });
 };
