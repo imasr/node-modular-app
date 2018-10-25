51ac9e3e1ce0a507d21899d6778ba81a66fd6c12
imasr <ashishs723@gmail.com>
2018-10-25 15:52:09 +0530

    revert

--- src/server.js
+++ src/server.js
@@ -1,32 +1,22 @@
 import './environment/environment';
 import express from "express";
-import crypto from "crypto";
-import https from "https";
-
-
 import cors from "cors";
 import { json } from "body-parser";
-import fs from "fs";
-
 import mongoose from "./db/mongoose";
-// import { ssl_middleware } from "./app/middleware/ssl.middleware"
 
 import { router } from "./app/modules/user/routes/user.routes";
 
 const port = process.env.PORT
-var privateKey = fs.readFileSync('privatekey.pem').toString();
-var certificate = fs.readFileSync('certificate.pem').toString();
-var credentials = crypto.createCredentials({ key: privateKey, cert: certificate });
 
 var app = express();
 app.use(json())
 app.use(cors())
+app.use(ssl_middleware);
 
 app.use('/', router)
 
-https.createServer(app)
-    .listen(port, () => {
-        console.log(`Server started at  ${port}, ${process.env.NODE_ENV}, ${process.env.MONGODB_URI}`);
-    })
+app.listen(port, () => {
+    console.log(`Server started at  ${port}, ${process.env.NODE_ENV}, ${process.env.MONGODB_URI}`);
+})
 
 
